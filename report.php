9a461436e559a224306223a2d174710320582333
Edgar Apaza <edgarapazac@gmail.com>
2014-04-21 10:59:40 -0500

    actualizando

--- report.php
+++ report.php
@@ -33,23 +33,35 @@ $pdf->Cell(20,10,'Total',1,1,'C');
 //
 //Consulta a Mysql
 //
-	$link = mysql_connect('192.168.1.100', 'usuario', 'archivo123') or die('No se pudo conectar: ' . mysql_error());
+	$link = mysql_connect('localhost', 'root', 'admin') or die('No se pudo conectar: ' . mysql_error());
 		//echo 'Connected successfully';
 	mysql_select_db('recepcion') or die('No se pudo seleccionar la base de datos');
 
 	// Realizar una consulta MySQL
-	$query = "SELECT r.numRec, r.numSol,r.total, r.fecha, d.concepto, CONCAT(u.nombre,' ',u.apePat,' ',apeMat) as nombres FROM recibo as r, detallerecibo as d , solicitudes as s, usuarios as u WHERE s.codUsu = u.codUsu and r.numSol = s.codSol and r.numRec = d.numRecibo and r.fecha LIKE '".$fecha."%' GROUP BY r.numRec;";
+	$query = "SELECT r.numRec, r.numSol, r.total, r.fecha, d.concepto, CONCAT(u.nombre,' ',u.apePat,' ',apeMat) as nombres, r.anulado FROM recibo as r, detallerecibo as d , solicitudes as s, usuarios as u WHERE s.codUsu = u.codUsu and r.numSol = s.codSol and r.numRec = d.numRecibo and r.fecha LIKE '".$fecha."%' GROUP BY r.numRec;";
 	$result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
 
 	$i =1;
 	while ($line = mysql_fetch_array($result)) {
-
-	    $pdf->Cell(10,6,$i,1,0,'C');  //Numeracion
-		$pdf->Cell(19,6,$line[0],1,0,'C'); //Recibo
-		$pdf->Cell(19,6,$line[1],1,0,'C'); //Solicitud
-		$pdf->Cell(35,6,$line[4],1,0,'L'); //concepto
-		$pdf->Cell(80,6,$line[5],1,0,'L'); // Nombres
-		$pdf->Cell(20,6,$line[2],1,1,'C'); // total
+		
+		if($line[6] == 1)
+		{
+		    $pdf->Cell(10,6,$i,1,0,'C');  //Numeracion
+			$pdf->Cell(19,6,$line[0],1,0,'C'); //Recibo
+			$pdf->Cell(19,6,$line[1],1,0,'C'); //Solicitud
+			$pdf->Cell(35,6,'ANULADO',1,0,'L'); //concepto
+			$pdf->Cell(80,6,$line[5],1,0,'L'); // Nombres
+			$pdf->Cell(20,6,$line[2],1,1,'C'); // total
+		}
+		else
+		{
+			$pdf->Cell(10,6,$i,1,0,'C');  //Numeracion
+			$pdf->Cell(19,6,$line[0],1,0,'C'); //Recibo
+			$pdf->Cell(19,6,$line[1],1,0,'C'); //Solicitud
+			$pdf->Cell(35,6,$line[4],1,0,'L'); //concepto
+			$pdf->Cell(80,6,$line[5],1,0,'L'); // Nombres
+			$pdf->Cell(20,6,$line[2],1,1,'C'); // total
+		}
 		$i++;
 	}
 
@@ -63,8 +75,8 @@ $pdf->Cell(20,10,'Total',1,1,'C');
 	mysql_close($link);
 	// Imprime la suma total
 	$pdf->SetFont("Arial",'B',12);
-	$pdf->Cell(163,10,"Total",0,0,'R'); // total
-	$pdf->Cell(20,10,$sumaTotal[0],1,1,'C'); // total
+	$pdf->Cell(163,10,'Total',0,0,'R'); // total
+	$pdf->Cell(20,10,round($sumaTotal[0],2),1,1,'C'); // total
 
 	$pdf->Footer();
 $pdf->Output();
